4bca3a3d8c38b22fce16136c5612160e484c6040
imvickychauhan <110216292+imvickychauhan@users.noreply.github.com>
2026-09-21 16:36:22 +0530

    Update statiCodeAnalysis.groovy
    changed maven plugin

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
-def call(credentialsId){
+def call(credentialsId) {
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
+        sh 'mvn clean package org.sonarsource.scanner.maven:sonar-maven-plugin:sonar'
     }
 }
