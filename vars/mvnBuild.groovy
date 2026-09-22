e1e842af710407d247a8c83bf947fabe017ce17b
imvickychauhan <110216292+imvickychauhan@users.noreply.github.com>
2026-09-22 17:26:48 +0530

    Update mvnBuild.groovy
    updatedd

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,5 +1,15 @@
 def call(){
-    withEnv(["JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64"]) {
-        sh 'mvn clean install -DskipTests'
-    }
+    sh '''
+        echo "USER=$(whoami)"
+        echo "JAVA_HOME=[$JAVA_HOME]"
+        echo "PATH=[$PATH]"
+        which java
+        readlink -f $(which java)
+        java -version
+        which mvn
+        mvn -version
+        ls -ld "$JAVA_HOME"
+        ls -l "$JAVA_HOME/bin/java"
+        mvn clean install -DskipTests
+    '''
 }
