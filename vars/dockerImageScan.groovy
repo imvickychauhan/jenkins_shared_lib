c96fa65da06ca475e2ae7841ef9ea0e4fbbf61e9
imvickychauhan <110216292+imvickychauhan@users.noreply.github.com>
2026-09-24 17:02:25 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,25 +1,10 @@
 def call(String project, String ImageTag, String hubUser){
-
-    sh """
-        trivy --config /dev/null \
-              --ignorefile /dev/null \
-              image \
-              --scanners vuln \
-              ${hubUser}/${project}:${ImageTag} > scan.txt
-
-        cat scan.txt
+    
+    sh """   
+     trivy image ${hubUser}/${project}:latest > scan.txt
+     cat scan.txt
     """
 }
-
-
-
-//def call(String project, String ImageTag, String hubUser){
-    
-//    sh """   
-//     trivy image ${hubUser}/${project}:latest > scan.txt
-//     cat scan.txt
-//    """
-//}
 
 // def call(String aws_account_id, String region, String ecr_repoName){
     
